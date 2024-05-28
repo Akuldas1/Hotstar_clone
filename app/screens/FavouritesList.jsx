4f09603475b7dff70b2b6d7a91e95ef10b2b83cc
Akuldas1 <akuldas651@gmail.com>
2024-05-28 11:02:36 +0530

    change

--- app/screens/FavouritesList.jsx
+++ app/screens/FavouritesList.jsx
@@ -1,67 +1,3 @@
-// import React, { useContext } from 'react';
-// import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
-// import { AntDesign } from '@expo/vector-icons';
-// import { FavoritesContext } from '../components/FavoritesContext';
-
-// const FavoritesList = () => {
-//   const { favorites, removeFromFavorites } = useContext(FavoritesContext);
-
-//   const renderFavoriteIcon = (movie) => {
-//     return (
-//       <TouchableOpacity onPress={() => removeFromFavorites(movie)} style={styles.favoriteIcon}>
-//         <AntDesign name="heart" size={24} color="yellow" />
-//       </TouchableOpacity>
-//     );
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.title}>Favorites</Text>
-//       <FlatList
-//         horizontal
-//         data={favorites}
-//         keyExtractor={(item) => item.id.toString()}
-//         renderItem={({ item }) => (
-//           <View style={styles.movieItem}>
-//             <Image source={{ uri: item.posterURL }} style={styles.image} />
-//             {renderFavoriteIcon(item)}
-//           </View>
-//         )}
-//       />
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 10,
-//     backgroundColor: 'black',
-//   },
-//   title: {
-//     fontSize: 24,
-//     color: 'white',
-//     fontFamily: 'Inter-Bold',
-//   },
-//   movieItem: {
-//     marginRight: 10,
-//     marginTop:10
-//   },
-//   image: {
-//     width: 150,
-//     height: 200,
-//     borderRadius: 10,
-//   },
-//   favoriteIcon: {
-//     position: 'absolute',
-//     top: 10,
-//     right: 10,
-//     zIndex: 1,
-//   },
-// });
-
-// export default FavoritesList;
-
 import React, { useContext } from 'react';
 import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
@@ -72,7 +8,6 @@ const FavoritesList = () => {
   const { favorites, removeFromFavorites } = useContext(FavoritesContext);
   const navigation = useNavigation(); 
 
-
   const renderFavoriteIcon = (movie) => {
     return (
       <TouchableOpacity onPress={() => removeFromFavorites(movie)} style={styles.favoriteIcon}>
@@ -80,7 +15,6 @@ const FavoritesList = () => {
       </TouchableOpacity>
     );
   };
-
 
   const navigateToMovieDetail = (movie) => {
     navigation.navigate('MovieDetail', { movie });
@@ -93,7 +27,7 @@ const FavoritesList = () => {
         vertical
         contentContainerStyle={styles.flatListContent}
         data={favorites}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item, index) => (item.id ? item.id.toString() : index.toString())}
         renderItem={({ item }) => (
           <TouchableOpacity onPress={() => navigateToMovieDetail(item)} style={styles.movie}>
             <Image source={{ uri: item.posterURL }} style={styles.image} />
